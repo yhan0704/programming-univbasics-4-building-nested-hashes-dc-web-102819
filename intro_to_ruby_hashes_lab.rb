9d241e5f0ae95c3665bc7825f861c223e5296c28
pleasant-controller-2669 <han20@umbc.edu>
2019-10-17 18:08:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {railroads:{pieces:4, rent_in_dollars:4}}
+  {railroads:{pieces:4, rent_in_dollars:{1,2,3,4}}}
 end
 
 def monopoly_with_fourth_tier
